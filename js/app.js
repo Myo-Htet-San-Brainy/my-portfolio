f1be55ace599c28dfca326ec64e050abe8d742c6
Myo-Htet-San-Brainy <myohtetsandrinksgit@gmail.com>
2023-09-07 17:00:51 +0700

    f

---
+++ js/app.js
@@ -0,0 +1,48 @@
+// SELECTORS
+const navbar = document.querySelector("nav");
+const openSidebarBtn = document.querySelector(".nav-header-toggle");
+const closeSidebarBtn = document.querySelector(".sidebar-close-btn");
+const sidebar = document.querySelector(".sidebar");
+const date = document.querySelector(".date");
+const facebookLinks = document.querySelectorAll(".fa-facebook");
+const instagramLinks = document.querySelectorAll(".fa-instagram");
+const linkedInLinks = document.querySelectorAll(".fa-linkedin");
+
+//DATA IMPORTS
+
+// LOGIC
+// FIXED NAVBAR
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+});
+
+// SIDEBAR
+openSidebarBtn.addEventListener("click", () => {
+  sidebar.classList.add("show-sidebar");
+});
+closeSidebarBtn.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
+
+// DATE
+date.textContent = new Date().getFullYear();
+
+// INCLUDE REAL LINKS IN SOCIAL ICONS IN FOOTERS AND SIDEBAR
+facebookLinks.forEach((link) => {
+  const parentLink = link.parentElement;
+  parentLink.setAttribute("href", "#");
+});
+
+instagramLinks.forEach((link) => {
+  const parentLink = link.parentElement;
+  parentLink.setAttribute("href", "#");
+});
+
+linkedInLinks.forEach((link) => {
+  const parentLink = link.parentElement;
+  parentLink.setAttribute("href", "#");
+});
---
+++ js/data.js
@@ -0,0 +1,5 @@
+export const mySocialLinks = {
+  fbLink: "https://www.facebook.com/myo.h.san.754",
+  instaLink: "https://www.instagram.com/brainythemilkdrinker_/",
+  linkedInLink: "https://www.linkedin.com/in/myo-htet-san-brainy-a4b680239/",
+};
